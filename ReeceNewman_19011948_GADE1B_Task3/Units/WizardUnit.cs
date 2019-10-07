70ba0be99dc1678fcb3b72b49bb648c85a2d36f7
SShadowRavenSS <47855765+SShadowRavenSS@users.noreply.github.com>
2019-10-07 08:35:48 +0200

    Created and implemented wizard class
    Added and implemented wizard class with all basic methods

---
+++ ReeceNewman_19011948_GADE1B_Task3/Units/WizardUnit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Units
+{
+    public class WizardUnit : Unit
+    {
+        public WizardUnit(int xPos, int yPos, int health, int speed, int range, char symbol, int attack, int faction, int maxHealth, string name) : base(xPos, yPos, health, faction, speed, attack, range, symbol, maxHealth, name)
+        {
+
+        }
+
+        //method that saves the unit into a file
+        public override void save()
+        {
+            FileStream fs = new FileStream("saves/units/saves.game", FileMode.Append, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(saveFormatter());
+
+            sw.Close();
+            fs.Close();
+        }
+
+        //Method that generates and returns a formatted string for saving purposes
+        private string saveFormatter()
+        {
+            string output = "";
+
+            output = XPos + "," + YPos + "," + Health + "," + Speed + "," + AttackRange + "," + Symbol + "," + Attack + "," + Faction + "," + MaxHealth + "," + Name;
+
+            return output;
+        }
+    }
+}
